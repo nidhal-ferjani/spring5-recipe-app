d97a6f3f6c713b3036f38a2a244606d34a9e9725
Nidhal Ferjani <nidhal.ferjani@gmail.com>
2019-06-06 21:41:37 +0100

    added Relation ManyToMany for Data JPA recipe Application

---
+++ src/main/java/com/springframework/domain/Category.java
@@ -0,0 +1,44 @@
+package com.springframework.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Created by Nidhal on 06/06/2019.
+ */
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
